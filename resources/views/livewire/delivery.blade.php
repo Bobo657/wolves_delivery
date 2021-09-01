0316f2455beb97d45e72675cf8c1b28720b84a51
Bobo657 <ukauwaj@gmail.com>
2021-09-01 19:58:21 +0100

    deliveries table added

--- resources/views/livewire/delivery.blade.php
+++ resources/views/livewire/delivery.blade.php
@@ -32,16 +32,30 @@
                     </td>
                     <td>{{ $delivery->created_at->format('Y-m-d') }}</td>
                     <td class="text-center">
-                        <div class="dropdown custom-dropdown">
-                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <svg> ... </svg>
+                    <div class="dropdown d-inline-block">
+                            <a class="dropdown-toggle" href="#" role="button" id="pendingTask" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
+                                    <circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle>
+                                    <circle cx="12" cy="19" r="1"></circle>
+                                </svg>
                             </a>
 
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink3">
-                                <a class="dropdown-item" href="javascript:void(0);">Download</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Share</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Edit</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                            <div class="dropdown-menu left" aria-labelledby="pendingTask" style="will-change: transform;">
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                   In Transit
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    Onhold
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    Pending
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    View 
+                                </a>
+                                <a class="dropdown-item" href="javascript:void(0);">
+                                    Delete
+                                </a>
                             </div>
                         </div>
                     </td>
