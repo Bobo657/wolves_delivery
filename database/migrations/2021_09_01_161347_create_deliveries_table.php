068e5d55b9c6efc5074fd482a394595f367745ae
Bobo657 <ukauwaj@gmail.com>
2021-09-01 17:27:04 +0100

    auth installed

---
+++ database/migrations/2021_09_01_161347_create_deliveries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('sender');
+            $table->string('email');
+            $table->string('address');
+            $table->string('image')->nullable();
+            $table->string('status');
+            $table->string('location');
+            $table->string('destination');
+            $table->string('tracking_number');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
